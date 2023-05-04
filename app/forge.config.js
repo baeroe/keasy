d2907be34d60c7b31ec8474378df1f78885a8bd2
Rafael  Hingerl <rafael@Rafaels-MBP.fritz.box>
2023-05-04 12:34:16 +0200

    make some changes

--- app/forge.config.js
+++ app/forge.config.js
@@ -11,5 +11,26 @@ module.exports = {
         draft: true
       }
     }
+  ],
+  packagerConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'electron_quick_start'
+      }
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
   ]
 }
